debffd3a6fe8bc49fc920364c561ee120690f837
moi23 <moisesboas@gmail.com>
2020-09-24 13:31:36 -0300

    chore: some items in loginPage

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,22 @@
 import React from "react";
-
 import * as S from "./styles";
+import LogoDivulgadores from "../../Assets/LogoDivulgadores.png";
 
 const Login = () => {
   return (
     <S.Container>
-      <S.Left></S.Left>
-      <S.Right></S.Right>
+      <S.Left />
+      <S.Right>
+        <div className="FatherBoxRight">
+          <img src={LogoDivulgadores} alt="Divulgadores Logo" />
+          <p>Informe seus dados de acesso</p>
+          <div id="ChildrenBoxInputRight">
+            <input type="text" placeholder="login do usuário" />
+            <input type="password" placeholder="senha do usuário" />
+            <button type="submit">ENTRAR</button>
+          </div>
+        </div>
+      </S.Right>
     </S.Container>
   );
 };
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -3,6 +3,7 @@ import BgImg from "../../Assets/bgLogin.png";
 
 export const Container = styled.div`
   display: flex;
+  background: #f8f8f8;
 `;
 
 export const Left = styled.div`
@@ -17,4 +18,52 @@ export const Right = styled.div`
   width: 720px;
   background: #f8f8f8;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+
+  .FatherBoxRight {
+    margin-top: 21%;
+
+    p {
+      color: #8e8e8e;
+      font-size: 31px;
+      font-weight: bold;
+      margin-top: 10px;
+      margin-bottom: 15px;
+    }
+
+    #ChildrenBoxInputRight {
+      display: flex;
+      flex-direction: column;
+
+      input {
+        margin-top: 30px;
+        height: 83px;
+        border-radius: 4px;
+        border: solid 2px #c2c2c2;
+        padding: 0 0 0 30px;
+        font-size: 27px;
+      }
+
+      input:focus {
+        outline: 2px solid #2aff71; /* oranges! yey */
+        border-radius: 4px;
+      }
+
+      input::placeholder {
+        font-size: 27px;
+        font-weight: 500;
+        color: #d0d0d0;
+      }
+
+      button {
+        color: white;
+        background: #00ab39;
+        height: 66px;
+        border: none;
+        margin-top: 45px;
+        border-radius: 4px;
+      }
+    }
+  }
 `;
