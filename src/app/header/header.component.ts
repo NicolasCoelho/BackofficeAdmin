7d552d8dd119d9415f00aa255260cf1e00c5e077
moi23 <moisesboas@gmail.com>
2020-10-27 15:13:55 -0300

    feat: msgbox system Info

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,38 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { faList, faUsers, faStore, faCopyright, faHome, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faList,
+  faUsers,
+  faStore,
+  faCopyright,
+  faHome,
+  faSignOutAlt,
+  faExclamation,
+} from '@fortawesome/free-solid-svg-icons';
 import { Authentication } from '../_services/authentication';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  public faHome= faHome;
+  public faHome = faHome;
   public faList = faList;
   public faUsers = faUsers;
   public faStore = faStore;
   public faCopy = faCopyright;
   public faSingOut = faSignOutAlt;
+  public faExclamation = faExclamation;
 
   public name: string;
 
   constructor(private router: Router, private auth: Authentication) {
-    this.name = this.auth.getTokenData().n
+    this.name = this.auth.getTokenData().n;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   goTo(target) {
-    this.router.navigate(['dashboard', target])
+    this.router.navigate(['dashboard', target]);
   }
 
   logOut() {
-    this.auth.deleteToken()
-    this.router.navigate(['/'])
+    this.auth.deleteToken();
+    this.router.navigate(['/']);
   }
 
+  callCard() {
+    let btn = document.querySelector('#btnInfo');
+    btn.classList.remove('btnInfoDispNone');
+  }
 }
