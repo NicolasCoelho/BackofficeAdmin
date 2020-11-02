d52bdec262f870d9dc064ad1535d95f234778e89
NicolasCoelho <nicolas_mansan@hotmail.com>
2020-11-02 16:51:24 -0300

    Values bind to model in store register

--- src/app/dashboard/register-store/register-store.component.ts
+++ src/app/dashboard/register-store/register-store.component.ts
@@ -31,6 +31,12 @@ export class RegisterStoreComponent implements OnInit {
   ];
   public enviroments: Array<Enviroment> = [];
 
+  public store = new Store()
+  public requirements = new UserRequirements()
+  public sale = new SaleStatus()
+  public contract = new Contract()
+  public user = new User()
+
   constructor(
     private ws: Ws,
     private router: Router,
@@ -86,69 +92,29 @@ export class RegisterStoreComponent implements OnInit {
   }
 
   register() {
-    // if (!this.storeForm.valid) {
-    //   alert('Dados incorretos');
-    //   return;
-    // }
+    if (!this.storeForm.valid) {
+      alert('Dados incorretos');
+      return;
+    }
 
-    let store = new Store();
-    let req = new UserRequirements()
-    let sale = new SaleStatus()
-    let contract = new Contract()
-    let user = new User()
+    this.contract.status = 1;
 
-    store.name = this.storeForm.get('storeName').value;
-    store.allow_register = this.storeForm.get('storeAllowRegister').value;
-    store.status = this.storeForm.get('storeStatus').value;
-    store.url = this.storeForm.get('storeUrl').value;
-    store.enviroment_id = this.storeForm.get('storeEnv').value;
-    store.website_id = this.storeForm.get('storeWebsite').value;
-    store.protected_register = this.storeForm.get('storeProtectedRegister').value;
-    store.comission_type = this.storeForm.get('storeComissionType').value;
-    store.comission_value = this.storeForm.get('storeComissionValue').value;
+    this.user.type = 3;
+    this.user.status = 1;
 
-    req.phone_1 = this.storeForm.get('reqPhone1').value;
-    req.phone_2 = this.storeForm.get('reqPhone2').value;
-    req.pis = this.storeForm.get('reqPis').value;
-    req.rg = this.storeForm.get('reqRg').value;
-    req.account = this.storeForm.get('reqAccount').value;
-    req.account_owner = this.storeForm.get('reqAccountOwner').value;
-    req.account_ownerCpf = this.storeForm.get('reqAccountOwnerCpf').value;
-    req.agency = this.storeForm.get('reqAgency').value;
-    req.bank = this.storeForm.get('reqBank').value;
-    req.birth_location = this.storeForm.get('reqBirthLocation').value;
-    req.birthdate = this.storeForm.get('reqBirthDate').value;
-    req.cpf_cnpj = this.storeForm.get('reqCpfCnpj').value;
-    req.gender = this.storeForm.get('reqGender').value;
-    req.literacy = this.storeForm.get('reqLiteracy').value;
-    req.marital_status = this.storeForm.get('reqMaritalStatus').value;
-    req.nationality = this.storeForm.get('reqNationality').value;
-    req.cep = this.storeForm.get('reqCep').value;
-    req.city = this.storeForm.get('reqCity').value;
-    req.adress = this.storeForm.get('reqAdress').value;
-    req.adress_number = this.storeForm.get('reqAdressNumber').value;
-    req.neighborhood = this.storeForm.get('reqNeighborhood').value;
-    req.state = this.storeForm.get('reqState').value;
+    Object.keys(this.requirements).forEach(
+      key => {
+        if(this.requirements[key] === undefined) this.requirements[key] = false
+      }
+    )
 
-    sale.received = this.storeForm.get('saleReceived').value;
-    sale.canceled = this.storeForm.get('saleCanceled').value;
-    sale.complete = this.storeForm.get('saleCompleted').value;
-
-    contract.content = this.storeForm.get('contract').value;
-    contract.status = 1;
-
-    user.name = this.storeForm.get('userName').value;
-    user.email = this.storeForm.get('userEmail').value;
-    user.password = this.storeForm.get('userPassword').value;
-    user.type = 3;
-    user.status = 1;
 
     let payload = {
-      store: store,
-      user_requirements: req,
-      sales_status: sale,
-      contract: contract,
-      default_user: user
+      store: this.store,
+      user_requirements: this.requirements,
+      sales_status: this.sale,
+      contract: this.contract,
+      default_user: this.user
     }
 
     console.log(payload);
