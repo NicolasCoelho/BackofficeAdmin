817902847af4397de27b4d41c67484bede6361eb
NicolasCoelho <nicolas_mansan@hotmail.com>
2021-05-31 20:19:06 -0300

    contract training and help page data in model

--- src/app/_models/contract.ts
+++ src/app/_models/contract.ts
@@ -1,6 +1,8 @@
 export class Contract {
   public id: number;
   public content: string;
+  public trainingPage: string;
+  public helpPape: string;
   public status: number;
   public createdAt: string;
   public updatedAt: string;
